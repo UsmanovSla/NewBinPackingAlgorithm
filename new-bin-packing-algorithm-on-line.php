d72f5a1114431d4eada78ec8f07973fea2a1f479
Vjaceslav <121697412+UsmanovSla@users.noreply.github.com>
2025-01-09 17:02:00 +0100

    Uploading src code of NBPA

---
+++ new-bin-packing-algorithm-on-line.php
@@ -0,0 +1,329 @@
+<?php
+
+ini_set('error_reporting', E_ERROR);
+ini_set("display_errors", 1);
+ini_set('memory_limit', '1024M');
+set_time_limit(120);
+
+
+
+echo "<h2>New Bin-Packing algorithm @ CTU in Prague, Faculty of Civil Engineering, K122, by Vjaceslav Usmanov</h2>";
+
+function checking ($variable){
+//	$variable = EregI_Replace("<", "&lt;", $variable);
+//	$variable = EregI_Replace(">", "&gt;", $variable);
+//	$variable = EregI_Replace('"', '&quot;', $variable);	
+//	$variable = EregI_Replace('\'', '&quot;', $variable);
+//	$variable=htmlspecialchars($variable,ENT_COMPAT,'cp1251');
+//	$variable=addslashes($variable);
+//	$variable=quotemeta($variable);
+//	$variable = round($variable);
+return $variable;
+}
+
+$run = checking ($_POST['run']);
+
+
+if (!$run){
+
+	echo "<form  name='form1' ACTION='' METHOD='post' ENCTYPE='multipart/form-data'>";
+	echo "<input type='hidden' name='run' value='1'>";
+	
+	echo "<table>";
+	
+	echo "<tr>";
+	echo "<td colspan='5'><em>Parameter of algorithm NBPA:</em></td>";
+	echo "</tr>";
+	
+	echo "<tr>";
+	echo "<td>Number of random variants: </td>";
+	echo "<td><input type='text' value='50' name='v' size='5'></td>";
+	echo "<td> (1 - 100)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td colspan='5'><br><em>Multicriterial analyze:</em></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Minimize number of containers: </td>";
+	echo "<td><input type='text' value='5' name='m1' size='5'></td>";
+	echo "<td> (0 - 9)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Minimize number of combinations: </td>";
+	echo "<td><input type='text' value='5' name='m2' size='5'></td>";
+	echo "<td> (0 - 9)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Minimize number of edges/cuts: </td>";
+	echo "<td><input type='text' value='5' name='m3' size='5'></td>";
+	echo "<td> (0 - 9)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Minimize unused space: </td>";
+	echo "<td><input type='text' value='5' name='m4' size='5'></td>";
+	echo "<td> (0 - 9)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td colspan='5'><br><em>Container:</em></td>";
+	echo "</tr>";
+	
+	echo "<tr>";
+	echo "<td>Size of container: </td>";
+	echo "<td><input type='text' value='500' name='container' size='5'></td>";
+	echo "<td> (1 - 1000)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Max number of parts in container: </td>";
+	echo "<td><input type='text' value='10' name='w' size='5'></td>";
+	echo "<td> (1 - 10)</td>";
+	echo "<td></td>";
+	echo "<td></td>";
+	echo "</tr>";
+	echo "</table>";
+
+	echo "<table>";
+	echo "<tr>";
+	echo "<td colspan='5'><br><em>Objects:</em></td>";
+	echo "</tr>";
+
+	
+	echo "<tr>";
+	echo "<td>Objekt 1: </td>";
+	echo "<td>Size <input type='text' value='300' name='size1' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='10' name='number1' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 2: </td>";
+	echo "<td>Size <input type='text' value='250' name='size2' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='5' name='number2' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 3: </td>";
+	echo "<td>Size <input type='text' value='150' name='size3' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='15' name='number3' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 4: </td>";
+	echo "<td>Size <input type='text' value='100' name='size4' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='20' name='number4' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 5: </td>";
+	echo "<td>Size <input type='text' value='0' name='size5' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number5' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 6: </td>";
+	echo "<td>Size <input type='text' value='0' name='size6' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number6' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	
+	echo "<tr>";
+	echo "<td>Object 7: </td>";
+	echo "<td>Size <input type='text' value='0' name='size7' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number7' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 8: </td>";
+	echo "<td>Size <input type='text' value='0' name='size8' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number8' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 9: </td>";
+	echo "<td>Size <input type='text' value='0' name='size9' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number9' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "<tr>";
+	echo "<td>Object 10: </td>";
+	echo "<td>Size <input type='text' value='0' name='size10' size='5'></td>";
+	echo "<td> ; </td>";
+	echo "<td>Number <input type='text' value='0' name='number10' size='5'></td>";
+	echo "<td> (1000 x 100)</td>";
+	echo "</tr>";
+
+	echo "</table>";
+
+	echo "<br><INPUT TYPE='submit' VALUE='Run Algorithm'></form>";
+}else{
+
+
+	$w = checking ($_POST['w']);
+	if ($w < 1) $w = 1;
+	if ($w > 10) $w = 10;
+
+	$v = checking ($_POST['v']);
+	if ($v < 1) $v = 1;
+	if ($v > 100) $v = 100;
+	
+	$max_variants = $v;
+
+	$max_number_parts_hard = $w;
+
+
+	$m1 = checking ($_POST['m1']);
+	$m2 = checking ($_POST['m2']);
+	$m3 = checking ($_POST['m3']);
+	$m4 = checking ($_POST['m4']);
+	
+	if (($m1<0)or($m1>9)) $m1 = 5;
+	if (($m2<0)or($m2>9)) $m2 = 5;
+	if (($m3<0)or($m3>9)) $m3 = 5;
+	if (($m4<0)or($m4>9)) $m4 = 5;
+	
+	
+	$container = checking ($_POST['container']);
+	if ($container < 1) $container = 1;
+	if ($container > 1000) $container = 1000;
+	
+	
+	$size1 = checking ($_POST['size1']);
+	if ($size1 < 0) $size1 = 0;
+	if ($size1 > 1000) $size1 = 1000;
+	$number1 = checking ($_POST['number1']);
+	if ($number1 < 0) $number1 = 0;
+	if ($number1 > 100) $number1 = 100;
+
+	$size2 = checking ($_POST['size2']);
+	if ($size2 < 0) $size2 = 0;
+	if ($size2 > 1000) $size2 = 1000;
+	$number2 = checking ($_POST['number2']);
+	if ($number2 < 0) $number2 = 0;
+	if ($number2 > 100) $number2 = 100;
+
+	$size3 = checking ($_POST['size3']);
+	if ($size3 < 0) $size3 = 0;
+	if ($size3 > 1000) $size3 = 1000;
+	$number3 = checking ($_POST['number3']);
+	if ($number3 < 0) $number3 = 0;
+	if ($number3 > 100) $number3 = 100;
+
+	$size4 = checking ($_POST['size4']);
+	if ($size4 < 0) $size4 = 0;
+	if ($size4 > 1000) $size4 = 1000;
+	$number4 = checking ($_POST['number4']);
+	if ($number4 < 0) $number4 = 0;
+	if ($number4 > 100) $number4 = 100;
+
+	$size5 = checking ($_POST['size5']);
+	if ($size5 < 0) $size5 = 0;
+	if ($size5 > 1000) $size5 = 1000;
+	$number5 = checking ($_POST['number5']);
+	if ($number5 < 0) $number5 = 0;
+	if ($number5 > 100) $number5 = 100;
+
+	$size6 = checking ($_POST['size6']);
+	if ($size6 < 0) $size6 = 0;
+	if ($size6 > 1000) $size6 = 1000;
+	$number6 = checking ($_POST['number6']);
+	if ($number6 < 0) $number6 = 0;
+	if ($number6 > 100) $number6 = 100;
+	
+	$size7 = checking ($_POST['size7']);
+	if ($size7 < 0) $size7 = 0;
+	if ($size7 > 1000) $size7 = 1000;
+	$number7 = checking ($_POST['number7']);
+	if ($number7 < 0) $number7 = 0;
+	if ($number7 > 100) $number7 = 100;
+
+	$size8 = checking ($_POST['size8']);
+	if ($size8 < 0) $size8 = 0;
+	if ($size8 > 1000) $size8 = 1000;
+	$number8 = checking ($_POST['number8']);
+	if ($number8 < 0) $number8 = 0;
+	if ($number8 > 100) $number8 = 100;
+
+	$size9 = checking ($_POST['size9']);
+	if ($size9 < 0) $size9 = 0;
+	if ($size9 > 1000) $size9 = 1000;
+	$number9 = checking ($_POST['number9']);
+	if ($number9 < 0) $number9 = 0;
+	if ($number9 > 100) $number9 = 100;
+
+	$size10 = checking ($_POST['size10']);
+	if ($size10 < 0) $size10 = 0;
+	if ($size10 > 1000) $size10 = 1000;
+	$number10 = checking ($_POST['number10']);
+	if ($number10 < 0) $number10 = 0;
+	if ($number10 > 100) $number10 = 100;
+
+	if ($size1>$container) $size1 = $container;
+	if ($size2>$container) $size2 = $container;
+	if ($size3>$container) $size3 = $container;
+	if ($size4>$container) $size4 = $container;
+	if ($size5>$container) $size5 = $container;
+	if ($size6>$container) $size6 = $container;
+	if ($size7>$container) $size7 = $container;
+	if ($size8>$container) $size8 = $container;
+	if ($size9>$container) $size9 = $container;
+	if ($size10>$container) $size10 = $container;
+	
+	$parts = Array();
+	if (($size1)and($number1)) $parts[$size1]=$number1;
+	if (($size2)and($number2)) $parts[$size2]=$number2;
+	if (($size3)and($number3)) $parts[$size3]=$number3;
+	if (($size4)and($number4)) $parts[$size4]=$number4;
+	if (($size5)and($number5)) $parts[$size5]=$number5;
+	if (($size6)and($number6)) $parts[$size6]=$number6;
+	if (($size7)and($number7)) $parts[$size7]=$number7;
+	if (($size8)and($number8)) $parts[$size8]=$number8;
+	if (($size9)and($number9)) $parts[$size9]=$number9;
+	if (($size10)and($number10)) $parts[$size10]=$number10;
+
+	// End
+	echo "<br>>>> Start algorithm <<<<br>";
+
+	include ("pila.php");
+		
+	// End
+	echo "<br><br>>>> End algorithm <<<<br>";
+
+}
+echo "<hr>";
+echo "<b>E-mail: vjacheslav.usmanov@fsv.cvut.cz</b>";
+?>
---
+++ pila.php
@@ -0,0 +1,470 @@
+<?php
+
+//////////////////////////
+//// Factorial
+function factorial($number){ 
+    $factorial = 1; 
+    for ($i = 1; $i <= $number; $i++){ 
+      $factorial = $factorial * $i; 
+    } 
+    return $factorial; 
+} 
+
+
+
+//////////////////////////
+//// Allowed combinations
+
+function combination ($arr,$m,$container){
+	
+	sort($arr);
+	
+	$n = Array();
+	for ($j=1;$j<=$m;$j++){
+		if ($j<=$m) $n[$j] = count($arr);
+	}
+	$combination = Array();
+	for ($i1=0;$i1<$n[1];$i1++){
+		$temp[0] = $arr[$i1];
+		
+		for ($i2=$i1;$i2<$n[2];$i2++){
+			$temp[1] = $arr[$i2];
+
+			for ($i3=$i2;$i3<$n[3];$i3++){
+				$temp[2] = $arr[$i3];
+
+				for ($i4=$i3;$i4<$n[4];$i4++){
+					$temp[3] = $arr[$i4];
+
+						for ($i5=$i4;$i5<$n[5];$i5++){
+							$temp[4] = $arr[$i5];
+
+								for ($i6=$i5;$i6<$n[6];$i6++){
+									$temp[5] = $arr[$i6];
+
+										for ($i7=$i6;$i7<$n[7];$i7++){
+											$temp[6] = $arr[$i7];
+
+											for ($i8=$i7;$i8<$n[8];$i8++){
+												$temp[7] = $arr[$i8];
+
+												for ($i9=$i8;$i9<$n[9];$i9++){
+													$temp[8] = $arr[$i9];
+
+													for ($i10=$i9;$i10<$n[10];$i10++){
+														$temp[9] = $arr[$i10];
+
+														if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[9]);}
+														else {unset ($temp[9]);break;}
+													}
+													if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[8]);}
+													else {unset ($temp[8]);break;}
+												}
+												if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[7]);}
+												else {unset ($temp[7]);break;}
+											}
+											if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[6]);}
+											else {unset ($temp[6]);break;}
+										}
+									if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[5]);}
+									else {unset ($temp[5]);break;}
+								}
+							if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[4]);}
+							else {unset ($temp[4]);break;}
+						}
+						if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[3]);}
+						else {unset ($temp[3]);break;}
+				}
+				if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[2]);}
+				else {unset ($temp[2]);break;}
+			}
+			if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[1]);}
+			else {unset ($temp[1]);break;}
+		}
+		if (array_sum($temp)<=$container) {array_push ($combination,$temp);unset ($temp[0]);}
+		else {unset ($temp[0]);break;}
+	}
+	return $combination;
+}
+
+//////////////////////////
+//// Sort of allowed combinations
+
+// $sort = 1   -  less weigth for same elements
+// $sort = 2   -  more weigth for same elements
+function sorting_container($combination,$sort){
+	
+	$combination_optimal = Array();
+	$sort_Array = Array();
+
+	foreach ($combination as $key => $val) {
+		$difference = (count($val) - count(array_unique($val)))/10 +  mt_rand(1,100)/100;
+		if ($sort==1) $sort_Array[$key] = array_sum($val) - $difference;
+		if ($sort==2) $sort_Array[$key] = array_sum($val) + $difference;
+	}
+	
+	arsort($sort_Array);
+	
+	foreach ($sort_Array as $key => $val) {
+		array_push($combination_optimal,$combination[$key]);
+	}
+	
+	return $combination_optimal;
+}
+
+//////////////////////////
+// Reducing combination according to parts
+
+function reducing_combination($combination_working,$parts_working){
+	
+	$parts = array_unique($parts_working);
+	
+
+	$parts_counts = array_count_values($parts_working);
+
+	foreach ($combination_working as $combination_key => $combination) {
+		$combination_counts = array_count_values($combination);
+		foreach ($parts as $part_key => $part) {
+			if ($combination_counts[$part]>$parts_counts[$part]) unset ($combination_working[$combination_key]);
+		}
+		
+		$combination_counts_uniq = array_unique($combination);
+		foreach ($combination_counts_uniq as $combination_counts_uniq_key => $combination_uniq) {
+			$key = array_search($combination_uniq, $parts_working);
+			if ($key === false) {
+				unset ($combination_working[$combination_key]);
+			}
+		}
+		
+	}
+	
+	return $combination_working;
+}
+
+//////////////////////////
+
+$act_time = microtime(true);
+
+
+// Preparing inputs
+krsort($parts);
+
+$part = array();
+foreach ($parts as $key => $val) {
+    array_push($part,$key);
+}
+
+$participating = array();
+
+
+// Step 1: Prepearing inputs
+echo "<br>Size of conteiner: ".$container."<br>";
+
+echo "<br>Sorting parts by decreasing size:<br>";
+foreach ($parts as $key => $val) {
+    echo "$key = $val<br>";
+}
+
+echo "<br>Max participating in container for every part:<br>";
+foreach ($parts as $key => $val) {
+	$participating[$key] = floor ($container/$key);
+}
+foreach ($participating as $key => $val) {
+    echo "$key = $val<br>";
+}
+
+$max_number_parts = max($participating);
+
+if ($max_number_parts>$max_number_parts_hard) $max_number_parts = $max_number_parts_hard;
+
+echo "Max number of parts in container: ".$max_number_parts."<br>";
+
+$list_parts =  array();
+
+for ($j=0;$j<count($part);$j++){
+	for ($i=0;$i<$participating[$part[$j]];$i++){
+		array_push($list_parts,$part[$j]);
+	}
+}
+
+// Step 2: All allowed combinations
+
+
+if ($max_number_parts == 0) {
+	echo "Error! Number of parts in container is 0!";
+	die();
+}
+
+if (count($parts) == 0) {
+	echo "Error! Number of objects is 0!";
+	die();
+}
+
+if (count($parts) > 20) {
+	echo "Error! Number of set of objects is too big!";
+	die();
+}
+
+//array_unshift($part,"");
+
+
+$allowed_combination = combination($part, $max_number_parts, $container);
+
+
+$combination_optimal = sorting_container($allowed_combination,1);
+
+
+$number_all_combination = factorial($max_number_parts + count($parts) -1)/factorial($max_number_parts)/factorial(count($parts) -1);
+
+echo "<br>Number of all combinations of container: ".$number_all_combination."<br>";
+
+echo "<br>Number of allowed combinations of container: ".count($combination_optimal)."<br>";
+
+$combination_working = $combination_optimal;
+
+$parts_working =  array();
+
+
+foreach ($parts as $key => $val) {
+	for ($i=0;$i<$val;$i++){
+		array_push($parts_working,$key);
+	}
+}
+
+
+$combination_working = reducing_combination($combination_working,$parts_working);
+
+echo "<br>Number of allowed reduced combinations of container: ".count($combination_working)."<br>";
+
+
+$total_w = Array();
+$total_cont = Array();
+$total_comb = Array();
+$total_cut = Array();
+$total_used = Array();
+$total_unused = Array();
+$total_percent = Array();
+$total_multicriterial = Array();
+
+
+for ($nar=0;$nar<$max_variants;$nar++){
+
+	// Step 3: Superior allowed combination
+	$combination_working = $combination_optimal;
+
+	$parts_working =  array();
+
+
+	foreach ($parts as $key => $val) {
+		for ($i=0;$i<$val;$i++){
+			array_push($parts_working,$key);
+		}
+	}
+
+	$combination_working = reducing_combination($combination_working,$parts_working);
+
+
+
+	// Step 4: Filling conteiners
+
+
+	$combination_result = Array();
+
+	$i = 0;
+
+	$parts_space = array_sum($parts_working);
+
+	$parts_total = count($parts_working);
+	$combination_total = count($combination_working);
+
+
+	while (count($parts_working)){
+
+		// Reducing combination according of parts
+		$combination_working = reducing_combination($combination_working,$parts_working);
+		// Shuffle of combination
+		$combination_working = sorting_container($combination_working,1);
+
+		
+		array_push($combination_result,$combination_working[0]);
+
+		foreach ($combination_working[0] as $key => $val) {
+			$key2 = array_search($val, $parts_working);
+			if ($key2 !== false) {
+				unset ($parts_working[$key2]);
+			}
+		}
+		$parts_rest[$i] = count($parts_working);
+		$combination_rest[$i] = count($combination_working);
+		
+		$i++; if ($i>500) break;
+		
+	}
+
+	$number_container = 0;
+	$used_space = 0;
+	foreach($combination_result as $val) {
+		$number_container++;
+		//echo $number_container.": ".array_sum($val)." = ".implode('+', $val), "<br>";
+		$used_space = $used_space + array_sum($val);
+	}
+	$container_space =  $number_container * $container;
+
+	echo "<hr>Variant: ".($nar+1)." > Space in containers: ".$container_space." / ";
+	echo "Used space: ".$used_space." / ";
+	echo "Parts space: ".$parts_space."<br><br>";
+
+
+	// Step 5: Output results
+	echo "<table border='1' cellpadding='2px' style='table-layout: fixed; border-collapse: collapse;'>";
+	echo "<tr>";
+	echo "<td align='center'>Container</td>";
+	echo "<td align='center'>Combination</td>";
+	echo "<td align='center'>Used place</td>";
+	echo "<td align='center'>Unused place</td>";
+	echo "<td align='center'>Combinations</td>";
+	echo "<td align='center'>Parts</td>";
+	echo "</tr>";
+	$number_container = 0;
+	$number_of_cutting = 0;
+
+	$combination_list = Array();
+
+	foreach($combination_result as $val) {
+		$number_container++;
+
+		$number_of_cutting = $number_of_cutting + count($val);
+		echo "<tr>";
+		echo "<td align='center'>".$number_container."</td>";
+		echo "<td align='left'>".implode('+', $val)."</td>";
+		echo "<td align='center'>".array_sum($val)."</td>";
+		echo "<td align='center'>".($container-array_sum($val))."</td>";
+		echo "<td align='center'>".$combination_rest[$number_container-1]."</td>";
+		echo "<td align='center'>".$parts_rest[$number_container-1]."</td>";
+		
+		echo "</tr>";
+		
+		array_push($combination_list,implode('+', $val));
+		if (($container-array_sum($val))==0) $number_of_cutting--;
+	}
+
+	$combination_result_unique = array_unique($combination_list);
+
+	echo "<tr>";
+	echo "<td align='center'><b>".count($combination_result)."</b></td>";
+	echo "<td align='center'><b>".count($combination_result_unique)." (Cuts: ".$number_of_cutting.")</b></td>";
+	echo "<td align='center'><b>".$used_space." (".number_format((($used_space/$container_space)*100),3,',',' ')."%)</b></td>";
+	echo "<td align='center'><b>".($container_space-$used_space)."</b></td>";
+	echo "<td align='center'><b>".$combination_total."</b></td>";
+	echo "<td align='center'><b>".$parts_total."</b></td>";
+	echo "</tr>";
+	echo "</table>";
+
+	array_push($total_w,$nar);
+	array_push($total_cont,count($combination_result));
+	array_push($total_comb,count($combination_result_unique));
+	array_push($total_cut,$number_of_cutting);
+	array_push($total_used,$used_space);
+	array_push($total_unused,($container_space-$used_space));
+	array_push($total_percent,number_format((($used_space/$container_space)*100),3,',',' '));
+
+}
+
+$total_cont_sort = array_unique($total_cont);
+sort($total_cont_sort);
+
+$total_cut_sort = array_unique($total_cut);
+sort($total_cut_sort);
+
+
+$total_unused_sort = array_unique($total_unused);
+sort($total_unused_sort);
+
+$total_comb_sort = array_unique($total_comb);
+sort($total_comb_sort);
+
+
+$total_m = $m1 + $m2 + $m3 + $m4; 
+
+
+for ($i=0;$i<count($total_w);$i++){
+	$total_multicriterial[$i] = 0;
+	
+	if ($total_m){
+		$key = array_search($total_cont[$i], $total_cont_sort);
+		if ($m1){
+			if ($m1!=$total_m){
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key * (1-$m1/$total_m);
+			}else{
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key;
+			}
+		}
+	
+		$key = array_search($total_comb[$i], $total_comb_sort);
+		if ($m2){
+			if ($m2!=$total_m){
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key * (1-$m2/$total_m);
+			}else{
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key;
+			}
+		}
+		
+
+		$key = array_search($total_cut[$i], $total_cut_sort);
+		if ($m3){
+			if ($m3!=$total_m){
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key * (1-$m3/$total_m);
+			}else{
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key;
+			}
+		}
+		
+		$key = array_search($total_unused[$i], $total_unused_sort);
+		if ($m4){
+			if ($m4!=$total_m){
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key * (1-$m4/$total_m);
+			}else{
+				if (false !== $key) $total_multicriterial[$i] = $total_multicriterial[$i] + $key;
+			}
+		}
+		
+	}
+	
+}
+
+
+echo "<hr>Results of algorithm for ".$max_variants." variants<br>";
+echo "<table border='1' cellpadding='2px' style='table-layout: fixed; border-collapse: collapse;'>";
+echo "<tr>";
+echo "<td>Variant</td>";
+echo "<td>Weight of criterial</td>";
+echo "<td>Comtainers</td>";
+echo "<td>Combinations</td>";
+echo "<td>Cuts</td>";
+echo "<td>Used place</td>";
+echo "<td>Unused place</td>";
+echo "<td>Percent usage</td>";
+echo "</tr>";
+
+
+asort ($total_multicriterial);
+
+foreach ($total_multicriterial as $key => $val) {
+	
+	echo "<tr>";
+	echo "<td align='center'>".($total_w[$key]+1)."</td>";
+	echo "<td align='center'>".round($total_multicriterial[$key],2)."</td>";
+	echo "<td align='center'>".$total_cont[$key]."</td>";
+	echo "<td align='center'>".$total_comb[$key]."</td>";
+	echo "<td align='center'>".$total_cut[$key]."</td>";
+	echo "<td align='center'>".$total_used[$key]."</td>";
+	echo "<td align='center'>".$total_unused[$key]."</td>";
+	echo "<td align='center'>".$total_percent[$key]." %</td>";
+	echo "</tr>";
+}
+echo "</tr>";
+echo "</table>";
+
+echo "Time: ".number_format(microtime(true) - $act_time,6,',','');
+
+?>
